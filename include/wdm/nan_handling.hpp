15eb034a00f2dc6832b434e06de1685ea4eca9d0
Thibault Vatter <thibault.vatter@gmail.com>
2025-09-01 14:21:39 +0200

    Add Chatterjee's Xi and format code

--- include/wdm/nan_handling.hpp
+++ include/wdm/nan_handling.hpp
@@ -13,63 +13,67 @@ namespace wdm {
 
 namespace utils {
 
-inline void remove_incomplete(std::vector<double>& x,
-                              std::vector<double>& y,
-                              std::vector<double>& w)
+inline void
+remove_incomplete(std::vector<double>& x,
+                  std::vector<double>& y,
+                  std::vector<double>& w)
 {
-    // if observation conatins nan, move it to the end
-    size_t last = x.size() - 1;
-    for (size_t i = 0; i < last + 1; i++) {
-        bool row_has_nan = (std::isnan(x[i]) || std::isnan(y[i]));
-        if (w.size() > 0)
-            row_has_nan = (row_has_nan || std::isnan(w[i]));
-        if (row_has_nan) {
-            if (w.size() > 0)
-                std::swap(w[i], w[last]);
-            std::swap(x[i], x[last]);
-            std::swap(y[i--], y[last--]);
-        }
-    }
-
-    x.resize(last + 1);
-    y.resize(last + 1);
+  // if observation conatins nan, move it to the end
+  size_t last = x.size() - 1;
+  for (size_t i = 0; i < last + 1; i++) {
+    bool row_has_nan = (std::isnan(x[i]) || std::isnan(y[i]));
     if (w.size() > 0)
-        w.resize(last + 1);
-}
-
-inline bool any_nan(const std::vector<double>& x) {
-    for (size_t i = 0; (i < x.size()); i++) {
-        if (std::isnan(x[i]))
-            return true;
+      row_has_nan = (row_has_nan || std::isnan(w[i]));
+    if (row_has_nan) {
+      if (w.size() > 0)
+        std::swap(w[i], w[last]);
+      std::swap(x[i], x[last]);
+      std::swap(y[i--], y[last--]);
     }
+  }
 
-    return false;
+  x.resize(last + 1);
+  y.resize(last + 1);
+  if (w.size() > 0)
+    w.resize(last + 1);
 }
 
-inline std::string preproc(std::vector<double>& x,
-                           std::vector<double>& y,
-                           std::vector<double>& weights,
-                           std::string method,
-                           bool remove_missing)
+inline bool
+any_nan(const std::vector<double>& x)
 {
-    size_t min_nobs = (method == "hoeffding") ? 5 : 2;
-    if (remove_missing) {
-        utils::remove_incomplete(x, y, weights);
-        if (x.size() < min_nobs)
-            return "return_nan";
-    } else {
-        std::stringstream msg;
-        if (utils::any_nan(x) || utils::any_nan(y) || utils::any_nan(weights)) {
-            msg << "there are missing values in the data; " <<
-                   "try remove_missing = TRUE";
-        } else if (x.size() < min_nobs) {
-            msg << "need at least " << min_nobs << "observations.";
-        }
-        if (!msg.str().empty())
-            throw std::runtime_error(msg.str());
-    }
+  for (size_t i = 0; (i < x.size()); i++) {
+    if (std::isnan(x[i]))
+      return true;
+  }
 
-    return "continue";
+  return false;
+}
+
+inline std::string
+preproc(std::vector<double>& x,
+        std::vector<double>& y,
+        std::vector<double>& weights,
+        std::string method,
+        bool remove_missing)
+{
+  size_t min_nobs = (method == "hoeffding") ? 5 : 2;
+  if (remove_missing) {
+    utils::remove_incomplete(x, y, weights);
+    if (x.size() < min_nobs)
+      return "return_nan";
+  } else {
+    std::stringstream msg;
+    if (utils::any_nan(x) || utils::any_nan(y) || utils::any_nan(weights)) {
+      msg << "there are missing values in the data; "
+          << "try remove_missing = TRUE";
+    } else if (x.size() < min_nobs) {
+      msg << "need at least " << min_nobs << "observations.";
+    }
+    if (!msg.str().empty())
+      throw std::runtime_error(msg.str());
+  }
+
+  return "continue";
 }
 
 } // end utils
--- include/wdm/ranks.hpp
+++ include/wdm/ranks.hpp
@@ -7,121 +7,121 @@
 #pragma once
 
 #include "nan_handling.hpp"
-#include "utils.hpp"
 #include "random.hpp"
+#include "utils.hpp"
 
 namespace wdm {
 
 namespace impl {
 
-  //! computes ranks.
-  //! @param x input vector.
-  //! @param weights (optional), weights for each observation.
-  //! @param ties_method `"min"` (default) assigns all tied values the minimum
-  //!   score; `"average"` assigns the average score, `"first"` ranks them in
-  //!   order of occurance, `"random"` randomizes.
-  //! @param seeds Seeds of the random number generator; if empty (default),
-  //!   the random number generator is seeded randomly.
-  //! @return a vector containing the ranks of each element in `x`.
-  inline std::vector<double>
-  rank(std::vector<double> x,
-       std::vector<double> weights = std::vector<double>(),
-       std::string ties_method = "min",
-       std::vector<int> seeds = std::vector<int>())
-  {
-    if ((ties_method != "min") && (ties_method != "average") &&
-        (ties_method != "first") && (ties_method != "random"))
-        throw std::runtime_error(
-          "ties method must be one of 'min', 'average', 'first', 'random'.");
+//! computes ranks.
+//! @param x input vector.
+//! @param weights (optional), weights for each observation.
+//! @param ties_method `"min"` (default) assigns all tied values the minimum
+//!   score; `"average"` assigns the average score, `"first"` ranks them in
+//!   order of occurance, `"random"` randomizes.
+//! @param seeds Seeds of the random number generator; if empty (default),
+//!   the random number generator is seeded randomly.
+//! @return a vector containing the ranks of each element in `x`.
+inline std::vector<double>
+rank(std::vector<double> x,
+     std::vector<double> weights = std::vector<double>(),
+     std::string ties_method = "min",
+     std::vector<int> seeds = std::vector<int>())
+{
+  if ((ties_method != "min") && (ties_method != "average") &&
+      (ties_method != "first") && (ties_method != "random"))
+    throw std::runtime_error(
+      "ties method must be one of 'min', 'average', 'first', 'random'.");
 
-    // set default weights if necessary
-    size_t n = x.size();
-    if (weights.size() == 0)
-        weights = std::vector<double>(n, 1.0);
+  // set default weights if necessary
+  size_t n = x.size();
+  if (weights.size() == 0)
+    weights = std::vector<double>(n, 1.0);
 
-    if (weights.size() != n) {
-        throw std::runtime_error("weights and data must have same size.");
-    }
-
-    // NaN-handling
-    std::vector<double> nans;
-    if (utils::any_nan(x)) {
-        nans.resize(n, 0);
-        for (size_t i = 0; i < n; i++) {
-            if (std::isnan(x[i])) {
-                x[i] = std::numeric_limits<double>::max();
-                nans[i] = 1;
-                weights[i] = 0;
-            }
-        }
-    }
-
-    double w_mean =
-      utils::sum(weights) / static_cast<double>(n - utils::sum(nans));
-    for (auto& w : weights) {
-        w = w / w_mean;
-    }
-
-    // permutation that brings 'x' in ascending order
-    std::vector<size_t> perm = utils::get_order(x);
-
-    double w_acc = 0.0, w_batch;
-    for (size_t i = 0, reps; i < n; i += reps) {
-        // find replications
-        reps = 0;
-        w_batch = 0.0;
-        while ((i + reps < n) && (x[perm[i]] == x[perm[i + reps]]))
-            w_batch += weights[perm[i + reps++]];
-
-        // assign min rank
-        for (size_t k = 0; k < reps; ++k)
-            x[perm[i + k]] = w_acc + weights[perm[i]];
-
-        // accumulate weights for current batch
-        w_acc += w_batch;
-
-        if (reps <= 1)
-            continue;
-
-        if (ties_method == "first") {
-            // assign weighted ranks in order of appearance
-            double ww = 0;
-            for (size_t k = 1; k < reps; ++k) {
-                ww += weights[perm[i + k]];
-                x[perm[i + k]] += ww;
-            }
-        } else if (ties_method == "random") {
-            // assign weighted ranks in random order
-            random::RandomGenerator random_gen(seeds);
-            std::vector<size_t> rvals(reps);
-            std::iota(rvals.begin(), rvals.end(), 0); // 0, 1, 2, ...
-            random::shuffle(rvals, random_gen);
-
-            double ww = 0;
-            for (size_t k = 1; k < reps; ++k) {
-                x[perm[i + rvals[k]]] += ww;
-                ww += weights[perm[i + rvals[k]]];
-            }
-        } else if (ties_method == "average") {
-            // assign average rank to tied values
-            std::vector<double> ww(reps);
-            for (size_t k = 0; k < reps; ++k)
-                ww[k] = weights[perm[i + k]];
-            for (size_t k = 0; k < reps; ++k)
-                x[perm[i + k]] += (utils::sum(ww) - weights[perm[i]]) / 2;
-        }
-    }
-
-    if (nans.size() == n) {
-        for (size_t i = 0; i < x.size(); i++) {
-            if (nans[i]) {
-                x[i] = NAN;
-            }
-        }
-    }
-
-    return x;
+  if (weights.size() != n) {
+    throw std::runtime_error("weights and data must have same size.");
   }
+
+  // NaN-handling
+  std::vector<double> nans;
+  if (utils::any_nan(x)) {
+    nans.resize(n, 0);
+    for (size_t i = 0; i < n; i++) {
+      if (std::isnan(x[i])) {
+        x[i] = std::numeric_limits<double>::max();
+        nans[i] = 1;
+        weights[i] = 0;
+      }
+    }
+  }
+
+  double w_mean =
+    utils::sum(weights) / static_cast<double>(n - utils::sum(nans));
+  for (auto& w : weights) {
+    w = w / w_mean;
+  }
+
+  // permutation that brings 'x' in ascending order
+  std::vector<size_t> perm = utils::get_order(x);
+
+  double w_acc = 0.0, w_batch;
+  for (size_t i = 0, reps; i < n; i += reps) {
+    // find replications
+    reps = 0;
+    w_batch = 0.0;
+    while ((i + reps < n) && (x[perm[i]] == x[perm[i + reps]]))
+      w_batch += weights[perm[i + reps++]];
+
+    // assign min rank
+    for (size_t k = 0; k < reps; ++k)
+      x[perm[i + k]] = w_acc + weights[perm[i]];
+
+    // accumulate weights for current batch
+    w_acc += w_batch;
+
+    if (reps <= 1)
+      continue;
+
+    if (ties_method == "first") {
+      // assign weighted ranks in order of appearance
+      double ww = 0;
+      for (size_t k = 1; k < reps; ++k) {
+        ww += weights[perm[i + k]];
+        x[perm[i + k]] += ww;
+      }
+    } else if (ties_method == "random") {
+      // assign weighted ranks in random order
+      random::RandomGenerator random_gen(seeds);
+      std::vector<size_t> rvals(reps);
+      std::iota(rvals.begin(), rvals.end(), 0); // 0, 1, 2, ...
+      random::shuffle(rvals, random_gen);
+
+      double ww = 0;
+      for (size_t k = 1; k < reps; ++k) {
+        x[perm[i + rvals[k]]] += ww;
+        ww += weights[perm[i + rvals[k]]];
+      }
+    } else if (ties_method == "average") {
+      // assign average rank to tied values
+      std::vector<double> ww(reps);
+      for (size_t k = 0; k < reps; ++k)
+        ww[k] = weights[perm[i + k]];
+      for (size_t k = 0; k < reps; ++k)
+        x[perm[i + k]] += (utils::sum(ww) - weights[perm[i]]) / 2;
+    }
+  }
+
+  if (nans.size() == n) {
+    for (size_t i = 0; i < x.size(); i++) {
+      if (nans[i]) {
+        x[i] = NAN;
+      }
+    }
+  }
+
+  return x;
+}
 
 //! computes ranks (such that smallest element has rank 0), assigning average
 //! ranks for ties.
@@ -130,48 +130,48 @@ namespace impl {
 //!   score; `"average"` assigns the average score.
 //! @param weights (optional), weights for each observation.
 //! @return a vector containing the ranks of each element in `x`.
-inline std::vector<double> rank0(
-    std::vector<double> x,
-    std::vector<double> weights = std::vector<double>(),
-    std::string ties_method = "min")
+inline std::vector<double>
+rank0(std::vector<double> x,
+      std::vector<double> weights = std::vector<double>(),
+      std::string ties_method = "min")
 {
-    if ((ties_method != "min") && (ties_method != "average"))
-        throw std::runtime_error("ties_method must be either 'min' or 'average.");
+  if ((ties_method != "min") && (ties_method != "average"))
+    throw std::runtime_error("ties_method must be either 'min' or 'average.");
 
-    // set default weights if necessary
-    size_t n = x.size();
-    if (weights.size() == 0)
-        weights = std::vector<double>(n, 1.0);
+  // set default weights if necessary
+  size_t n = x.size();
+  if (weights.size() == 0)
+    weights = std::vector<double>(n, 1.0);
 
-    // permutation that brings 'x' in ascending order
-    std::vector<size_t> perm = utils::get_order(x);
+  // permutation that brings 'x' in ascending order
+  std::vector<size_t> perm = utils::get_order(x);
 
-    double w_acc = 0.0, w_batch;
-    for (size_t i = 0, reps; i < n; i += reps) {
-        // find replications
-        reps = 0;
-        w_batch = 0.0;
-        while ((i + reps < n) && (x[perm[i]] == x[perm[i + reps]]))
-            w_batch += weights[perm[i + reps++]];
+  double w_acc = 0.0, w_batch;
+  for (size_t i = 0, reps; i < n; i += reps) {
+    // find replications
+    reps = 0;
+    w_batch = 0.0;
+    while ((i + reps < n) && (x[perm[i]] == x[perm[i + reps]]))
+      w_batch += weights[perm[i + reps++]];
 
-        // assign min rank
-        for (size_t k = 0; k < reps; ++k)
-            x[perm[i + k]] = w_acc;
+    // assign min rank
+    for (size_t k = 0; k < reps; ++k)
+      x[perm[i + k]] = w_acc;
 
-        // accumulate weights for current batch
-        w_acc += w_batch;
+    // accumulate weights for current batch
+    w_acc += w_batch;
 
-        // assign average rank to tied values
-        if ((ties_method == "average") && (reps > 1)) {
-            std::vector<double> ww(reps);
-            for (size_t k = 0; k < reps; ++k)
-                ww[k] = weights[perm[i + k]];
-            for (size_t k = 0; k < reps; ++k)
-                x[perm[i + k]] += utils::perm_sum(ww, 2) / w_batch;
-        }
+    // assign average rank to tied values
+    if ((ties_method == "average") && (reps > 1)) {
+      std::vector<double> ww(reps);
+      for (size_t k = 0; k < reps; ++k)
+        ww[k] = weights[perm[i + k]];
+      for (size_t k = 0; k < reps; ++k)
+        x[perm[i + k]] += utils::perm_sum(ww, 2) / w_batch;
     }
+  }
 
-    return x;
+  return x;
 }
 
 //! computes the bivariate rank of a pair of vectors (starting at 0).
@@ -183,29 +183,29 @@ bivariate_rank(std::vector<double> x,
                std::vector<double> y,
                std::vector<double> weights = std::vector<double>())
 {
-    utils::check_sizes(x, y, weights);
+  utils::check_sizes(x, y, weights);
 
-    // get inverse of permutation that brings x in ascending order
-    std::vector<size_t> perm_x = utils::get_order(x);
-    perm_x = utils::invert_permutation(perm_x);
+  // get inverse of permutation that brings x in ascending order
+  std::vector<size_t> perm_x = utils::get_order(x);
+  perm_x = utils::invert_permutation(perm_x);
 
-    // sort x, y, and weights according to x, breaking ties with y
-    utils::sort_all(x, y, weights);
+  // sort x, y, and weights according to x, breaking ties with y
+  utils::sort_all(x, y, weights);
 
-    // get inverse of permutation that brings y in descending order
-    std::vector<size_t> perm_y = utils::get_order(y, false);
-    perm_y = utils::invert_permutation(perm_y);
+  // get inverse of permutation that brings y in descending order
+  std::vector<size_t> perm_y = utils::get_order(y, false);
+  perm_y = utils::invert_permutation(perm_y);
 
-    // sort y in descending order counting inversions
-    std::vector<double> counts(y.size(), 0.0);
-    utils::merge_sort_count_per_element(y, weights, counts);
+  // sort y in descending order counting inversions
+  std::vector<double> counts(y.size(), 0.0);
+  utils::merge_sort_count_per_element(y, weights, counts);
 
-    // bring counts back in original order
-    std::vector<double> counts_tmp = counts;
-    for (size_t i = 0; i < counts.size(); i++)
-        counts[i] = counts_tmp[perm_y[perm_x[i]]];
+  // bring counts back in original order
+  std::vector<double> counts_tmp = counts;
+  for (size_t i = 0; i < counts.size(); i++)
+    counts[i] = counts_tmp[perm_y[perm_x[i]]];
 
-    return counts;
+  return counts;
 }
 
 //! computes the (weighted) median of a vector.
@@ -214,34 +214,34 @@ inline double
 median(const std::vector<double>& x,
        std::vector<double> weights = std::vector<double>())
 {
-    utils::check_sizes(x, x, weights);
-    size_t n = x.size();
+  utils::check_sizes(x, x, weights);
+  size_t n = x.size();
 
-    // sort x and weights in x order
-    auto perm = utils::get_order(x);
-    auto xx = x;
-    auto w = weights;
-    for (size_t i = 0; i < n; i++) {
-        xx[i] = x[perm[i]];
-        if (w.size() > 0)
-            w[i] = weights[perm[i]];
-    }
+  // sort x and weights in x order
+  auto perm = utils::get_order(x);
+  auto xx = x;
+  auto w = weights;
+  for (size_t i = 0; i < n; i++) {
+    xx[i] = x[perm[i]];
+    if (w.size() > 0)
+      w[i] = weights[perm[i]];
+  }
 
-    // compute weighted ranks and the "average rank" (corresponds to the
-    // median)
-    auto ranks = rank0(xx, w, "average");
-    if (weights.size() == 0)
-        weights = std::vector<double>(n, 1.0);
-    double rank_avrg = utils::perm_sum(weights, 2) / utils::sum(weights);
+  // compute weighted ranks and the "average rank" (corresponds to the
+  // median)
+  auto ranks = rank0(xx, w, "average");
+  if (weights.size() == 0)
+    weights = std::vector<double>(n, 1.0);
+  double rank_avrg = utils::perm_sum(weights, 2) / utils::sum(weights);
 
-    // weighted median splits data below and above rank_avrg
-    size_t i = 0;
-    while (ranks[i] < rank_avrg)
-        i++;
-    if (ranks[i] == rank_avrg)
-        return xx[i];
-    else
-        return 0.5 * (xx[i - 1] + xx[i]);
+  // weighted median splits data below and above rank_avrg
+  size_t i = 0;
+  while (ranks[i] < rank_avrg)
+    i++;
+  if (ranks[i] == rank_avrg)
+    return xx[i];
+  else
+    return 0.5 * (xx[i - 1] + xx[i]);
 }
 }
 }
